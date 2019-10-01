827addcdd475d8e4d01fbabbeb552e038638bd0d
Oscar Cortes <oscarcortesc12@gmail.com>
2019-10-01 15:26:18 -0600

    Session class
    Implemented a session class, as a singleton, to store data that is unique per session

---
+++ RestaurantesAKIP1/app/src/main/java/com/example/restaurantesakip1/Models/Session.java
@@ -0,0 +1,21 @@
+package com.example.restaurantesakip1.Models;
+
+/**
+ * Just a class to hold up the session data
+ * it's, of course, a Singleton
+ */
+public class Session {
+
+    public User user = null;
+    public String token = null;
+
+    private Session session = new Session();
+
+    private Session(){
+
+    }
+
+    public Session getInstace(){
+        return session;
+    }
+}
--- RestaurantesAKIP1/app/src/main/java/com/example/restaurantesakip1/Models/User.java
+++ RestaurantesAKIP1/app/src/main/java/com/example/restaurantesakip1/Models/User.java
@@ -1,4 +1,22 @@
 package com.example.restaurantesakip1.Models;
 
+import com.google.gson.annotations.SerializedName;
+
 public class User {
+
+    @SerializedName("_id")
+    public String _id;
+
+    @SerializedName("name")
+    public String name;
+
+    @SerializedName("lastName1")
+    public String lastName1;
+
+    @SerializedName("lastName2")
+    public String lastName2;
+
+    @SerializedName("email")
+    public String email;
+
 }
